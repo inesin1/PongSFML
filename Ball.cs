a6e760d8303de596170269969bf4ba8bc2304782
artem <artem.nesin@gmail.com>
2022-08-20 07:00:00 +0600

    Движение мяча

--- Ball.cs
+++ Ball.cs
@@ -18,6 +18,7 @@ namespace PongSFML
         public Sprite Sprite { get; set; } = new Sprite();          // Спрайт
 
         private float _speed = 400.0f;      // Скорость движения
+        private float _direction = 45.0f;   // Направление
 
         /// <summary>
         /// Инициализация. Запускается при создании объекта
@@ -33,6 +34,17 @@ namespace PongSFML
         /// </summary>
         public void Update(float deltaTime) {
             Sprite.Position = Position;
+
+            float newX = Position.X + (float)Math.Cos(DegToRad(_direction)) * _speed * deltaTime;
+            float newY = Position.Y - (float)Math.Sin(DegToRad(_direction)) * _speed * deltaTime;
+            Position = new Vector2f(newX, newY);
+        }
+
+        /// <summary>
+        /// Конвертирует градусы в радианы
+        /// </summary>
+        private float DegToRad(float deg) {
+            return deg * (float)Math.PI / 180;
         }
     }
 }
